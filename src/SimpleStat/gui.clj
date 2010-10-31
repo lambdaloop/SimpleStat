b955262d817f57d7fdc308e39f35083b58971327
Pierre <krchtchk@gmail.com>
2010-10-30 20:26:41 -0400

    Can now import data from lisp files and copy results from text field

--- src/SimpleStat/gui.clj
+++ src/SimpleStat/gui.clj
@@ -1,7 +1,8 @@
 ;(ns SimpleStat.gui)
 
 (import '(javax.swing JFrame JPanel JButton
-		      JComboBox JFileChooser JOptionPane)
+		      JComboBox JFileChooser JOptionPane
+		      SwingUtilities JTextArea)
         '(java.awt GridBagLayout GridBagConstraints Insets))
 (use '(clojure.contrib
        [swing-utils :only (add-action-listener)])
@@ -28,7 +29,6 @@
 		   ;; ... more components & constraints ...
 		   )
 
-
 (defmacro set-prop-gui!
   [constraints object properties]
   (cons
@@ -51,69 +51,90 @@
    (list `(. ~container validate))))
 
 
-(defn dialog-box [message]
+(defn dialog-box [& message]
   (JOptionPane/showMessageDialog
    nil
-   message))
+   (apply str message)))
+
 
 (defn printable-map [hmap]
-  (str "<html>"
-       (apply
-	str
-	(for [[key val] hmap]
-	  (str (name key) ":" \tab val "<br>")))
-       "</html>"))
+  (apply
+   str
+   (for [[key val] hmap]
+     (str (name key) ":  "  val \newline))))
+    
 		    
 
-(def frame)
-(def data)
-(def test-selected)
+(def frame (ref nil))
+(def text-area (ref nil))
+(def data (ref '((1 2 3) (4 5 6 7))))
+(def test-selected (ref "2 Sample T Test"))
 
+
+(defn show-message [& message]
+  (. @text-area (setText (apply str message))))
+   
 (defn import-data
   [event]
-  (let [file (.getSelectedFile (doto (JFileChooser.)
-				 (.showOpenDialog frame)))]
-    (if file
-      (dialog-box (slurp file)))))
+  (if-let [file (.getSelectedFile (doto (JFileChooser.)
+				 (.showOpenDialog nil)))]
+    (dosync (ref-set data (load-string (slurp file)))
+	    (dialog-box @data))))
       
 
 (defn test-data
   [event]
-  (let [selection (deref *test-selected*)]
-    (dialog-box
-     (printable-map (apply
-	   (case selection
-		 "2 Sample T Test" two-sample-t-test
-		 "ANOVA"  ANOVA)
-	   data)))))
+  (show-message
+   (printable-map (apply
+		   (case @test-selected
+			 "2 Sample T Test" two-sample-t-test
+			 "ANOVA"  ANOVA)
+		   @data))))
 
 (defn select-test
   [event]
-  (set!  test-selected
-	  (.. event getSource getSelectedItem))
-  (dialog-box (.. event getSource getSelectedItem) " selected!"))
+  (dosync
+   (ref-set test-selected
+	    (.. event getSource getSelectedItem))))
 
 (defn show-gui []
-  (binding [frame (JFrame. "Test Data!")
-	    data '((1 2 3) (4 5 6 7))
-	    test-selected "2 Sample T Test"]
-    
-    
-    (grid-bag-layout (. frame getContentPane)
-		     [:weightx 0.5]
-		     [(JButton. "Import")
-		      :listener import-data
-		      :gridwidth 2 :anchor :PAGE_START
-		      :gridx 0 :gridy 0]
-		     [(JComboBox. (into-array
-				   (list "2 Sample T Test" "ANOVA")))
-		      :listener select-test
-		      :gridx 0, :gridy 1]
-		     [(JButton. "TEST!")
-		      :listener test-data
-		      :gridx 1, :gridy 1])
-    (. frame (pack))
-    (. frame (setVisible true))))
+  (dosync (ref-set frame (JFrame. "Test Data!"))
+	  #_(ref-set data '((1 2 3) (4 5 6 7)))
+	  #_(ref-set test-selected "2 Sample T Test")
+	  (ref-set text-area (doto (JTextArea.)
+			       (.setLineWrap true)
+			       (.setWrapStyleWord true)
+			       (.setEditable false)
+			       (.setText "Data will appear here\n\n\n"))))
+	  (grid-bag-layout (. @frame getContentPane)
+			   [:fill :HORIZONTAL
+			    :weighty 1]
+			   [(JButton. "TEST!")
+			    :listener test-data
+			    :gridx 1, :gridy 1]
+			   [(JButton. "Import")
+			    :listener import-data
+			    :gridwidth 2 
+			    :gridx 0 :gridy 0
+			    :anchor :NORTH]
+			   [(JComboBox. (into-array
+					 (list "2 Sample T Test" "ANOVA")))
+			    :listener select-test
+			    :gridx 0, :gridy 1]
+			   [@text-area
+			    :gridx 0, :gridy 2
+			    :gridwidth 2 :gridheight 1
+			    :fill :BOTH
+			    :weighty 0.1
+			    :insets (Insets. 5 5 5 5)])
+	  (doto @frame
+	    (.pack)
+	    (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
+	    (.setSize 200 150)
+	    (.setVisible true)
+	    (.validate)))
 
 
-(SwingUtilites/invokeLater show-gui)
+(SwingUtilities/invokeLater show-gui)
+  
+;(. (Thread. show-gui) start)
